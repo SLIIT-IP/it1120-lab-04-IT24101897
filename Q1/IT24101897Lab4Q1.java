584a71526d347734c5ccdd2f577c39ddb4f61d9f
IT24101897 <IT24101897@my.sliit.lk>
2024-08-21 11:35:19 +0530

    Add files via upload
     Compute the smallest and largest of these numbers.

---
+++ Q1/IT24101897Lab4Q1.java
@@ -0,0 +1,30 @@
+import  java.util.Scanner;
+
+public class IT24101897Lab4Q1 {
+
+   public static void main(String[] args){
+
+         Scanner input = new Scanner(System.in);
+
+         System.out.print("Enter a number: ");
+         int number =input.nextInt();
+
+         System.out.println();
+
+         if(number > 0)
+          {
+           System.out.println("The number is: Positive");
+          }
+
+         else if (number < 0)
+          {
+           System.out.println("The number is: Negative");
+          }
+
+         else
+          {
+           System.out.println("The number is: zero");
+          }
+
+      }
+}
